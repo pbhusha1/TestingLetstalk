b23273c9561ecb7d1819c5a119370dfded8c8fa6
SriRamaD <sdonga@asu.edu>
2023-02-10 20:59:47 -0700

    Working project folder with Content View

---
+++ Let'sTalk_MobileApp_Team10/Let'sTalk_MobileApp_Team10/Let_sTalk_MobileApp_Team10App.swift
@@ -0,0 +1,17 @@
+//
+//  Let_sTalk_MobileApp_Team10App.swift
+//  Let'sTalk_MobileApp_Team10
+//
+//  Created by Krishna Sai  on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Let_sTalk_MobileApp_Team10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
